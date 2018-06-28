8d2c6a0c94e4243adfe76086623e6b83f6401c08
Sankar Rengarajan <mulshankar@gmail.com>
2018-06-28 14:37:50 -0500

    minor tweaks..

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,46 +26,42 @@ LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this n
 MAX_DECEL=0.5 # setting some deceleration rate for the car
 
 class WaypointUpdater(object):
-    def __init__(self):
+	def __init__(self):
 
-        rospy.init_node('waypoint_updater')
+		rospy.init_node('waypoint_updater')
 
-        # All subscribers are here
-        
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
-        
-        # Way point publisher
+		# All subscribers are here
+		
+		rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+		rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+		rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+		
+		# Way point publisher
 
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+		self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # Other member variables you need below
+		# Other member variables you need below
 
-        self.pose = None
-        self.frame_id = None
-        self.base_lane = None
-        self.waypoints_2d = None
-        self.waypoint_tree = None
-
-        self.loop()
+		self.pose = None
+		self.base_lane = None
+		self.waypoints_2d = None
+		self.waypoint_tree = None
+		self.loop()
 
     def loop(self):
         rate = rospy.Rate(40) # 40 Hz loop.. waypoint follower running at 30 Hz.. needs to run at least as fast 
         while not rospy.is_shutdown():
             if self.pose and self.base_lane and self.waypoint_tree:
-                closest_waypoint_idx=self.get_closest_waypoint_id()
-                self.publish_waypoints(closest_waypoint_idx)
+                self.publish_waypoints()
             rate.sleep()
 
     def pose_cb(self, msg):
         self.pose = msg
         
     def waypoints_cb(self, waypoints): #setting up KDTree with waypoints
-        self.base_waypoints = waypoints
+        self.base_lane = waypoints
         if not self.waypoints_2d:
-            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] \
-                                for waypoint in waypoints.waypoints]
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d) # setting up KDTree to identify nearest neighbour - log(n) vs N operation
 
     def get_closest_waypoint_id(self):
@@ -82,7 +78,7 @@ class WaypointUpdater(object):
         cl_vect = np.array(closest_coord)
         prev_vect = np.array(prev_coord)
         pos_vect = np.array([x, y])
-
+		
         val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
         
         if val > 0:
@@ -90,9 +86,9 @@ class WaypointUpdater(object):
 
         return closest_idx
 
-    def publish_waypoints(self,closest_idx):
-        final_lane=self.generate_Lane()
-        self.final_waypoints_pub.publish(final_lane)
+    def publish_waypoints(self):
+		final_lane=self.generate_Lane()
+		self.final_waypoints_pub.publish(final_lane)
 
     def generate_Lane(self):
 
@@ -118,7 +114,7 @@ class WaypointUpdater(object):
 
             stop_idx=max(self.stopline_wp_idx - closest_idx -2, 0) # subtract two way-points to ensure nose of car stops at line
             dist=self.distance(waypoints,i,stop_idx) # calculate distance from current index to stop line index
-            vel=2*MAX_DECEL*dist # some deceleration rate based on distance
+            vel=2.0*MAX_DECEL*dist # some deceleration rate based on distance
             if vel<1.0:
                 vel=0.0
             p.twist.twist.linear.x=min(vel,wp.twist.twist.linear.x) #set way point velocity to min of calculated vs speed limit (default value)
